eab92113699f5d112e95c4a5534f554524e79f3e
Alex Angelini <alex.louis.angelini@gmail.com>
2016-02-18 22:38:20 -0500

    Fix select results in execution

--- src/exec.rs
+++ src/exec.rs
@@ -1,8 +1,15 @@
 use data::{ColumnName, Db, Entries, Entry, Value};
 use query::{Comparator, Plan, Lhs, QueryNode, Rhs};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-type Cache = HashMap<ColumnName, Entries>;
+type Eids = HashSet<usize>;
+
+type Cache = HashMap<ColumnName, Eids>;
+
+enum Filtered {
+    Entries(Entries),
+    Eids(Eids),
+}
 
 #[derive(Debug)]
 pub enum Error {
@@ -19,52 +26,104 @@ fn compare(left: &Value, comp: &Comparator, right: &Value) -> bool {
     }
 }
 
-fn filter_entries(entries: &Entries, comp: &Comparator, value: &Value) -> Entries {
+fn find_eids_by_value(entries: &Entries, comp: &Comparator, value: &Value) -> Eids {
+    let mut eids = Eids::new();
+
     match *entries {
-        Entries::Bool(ref rows) => {
-            Entries::Bool(rows.iter().filter(|r| {
-                compare(&Value::Bool(r.value), comp, value)
-            }).cloned().collect::<Vec<Entry<bool>>>())
+        Entries::Bool(ref entries) => {
+            for entry in entries {
+                if compare(&Value::Bool(entry.value), comp, value) {
+                    eids.insert(entry.eid);
+                }
+            }
         }
-        Entries::Int(ref rows) => {
-            Entries::Int(rows.iter().filter(|r| {
-                compare(&Value::Int(r.value), comp, value)
-            }).cloned().collect::<Vec<Entry<usize>>>())
+        Entries::Int(ref entries) => {
+            for entry in entries {
+                if compare(&Value::Int(entry.value), comp, value) {
+                    eids.insert(entry.eid);
+                }
+            }
         }
-        Entries::String(ref rows) => {
-            Entries::String(rows.iter().filter(|r| {
-                compare(&Value::String(r.value.to_owned()), comp, value)
-            }).cloned().collect::<Vec<Entry<String>>>())
+        Entries::String(ref entries) => {
+            for entry in entries {
+                if compare(&Value::String(entry.value.to_owned()), comp, value) {
+                    eids.insert(entry.eid);
+                }
+            }
+        }
+    }
+
+    eids
+}
+
+fn match_by_eids<T>(entries: &[Entry<T>], eids: &Eids) -> Eids {
+    entries.iter()
+           .fold(Eids::new(), |mut acc, entry| {
+               if eids.contains(&entry.eid) {
+                   acc.insert(entry.eid);
+               }
+               acc
+           })
+}
+
+fn find_eids_by_set(entries: &Entries, eids: &Eids) -> Eids {
+    match *entries {
+        Entries::Bool(ref entries) => match_by_eids(entries, eids),
+        Entries::Int(ref entries) => match_by_eids(entries, eids),
+        Entries::String(ref entries) => match_by_eids(entries, eids),
+    }
+}
+
+fn clone_matching_entries<T: Clone>(entries: &[Entry<T>], eids: &Eids) -> Vec<Entry<T>> {
+    entries.iter()
+           .filter(|entry| eids.contains(&entry.eid))
+           .cloned()
+           .collect()
+}
+
+fn find_entries_by_set(entries: &Entries, eids: &HashSet<usize>) -> Entries {
+    match *entries {
+        Entries::Bool(ref entries) => Entries::Bool(clone_matching_entries(entries, eids)),
+        Entries::Int(ref entries) => Entries::Int(clone_matching_entries(entries, eids)),
+        Entries::String(ref entries) => Entries::String(clone_matching_entries(entries, eids)),
+    }
+}
+
+fn find_entries(db: &Db, cache: &Cache, node: &QueryNode) -> Result<(ColumnName, Filtered), Error> {
+    match *node {
+        QueryNode::Select(ref name) => {
+            let name_eid = name.eid();
+            let eids = try!(cache.get(&name_eid).ok_or(Error::MissingColumn(name_eid)));
+            let column = try!(db.cols.get(name).ok_or(Error::MissingColumn(name.to_owned())));
+
+            Ok((name.to_owned(),
+                Filtered::Entries(find_entries_by_set(&column.entries, &eids))))
+        }
+        QueryNode::Where(Lhs::Column(ref left), ref comp, Rhs::Constant(ref v)) => {
+            let left_eid = left.eid();
+            let column = try!(db.cols.get(left).ok_or(Error::MissingColumn(left.to_owned())));
+
+            Ok((left_eid,
+                Filtered::Eids(find_eids_by_value(&column.entries, comp, v))))
+        }
+        QueryNode::Where(Lhs::Column(ref left), _, Rhs::Column(ref right)) => {
+            let (left_eid, right_eid) = (left.eid(), right.eid());
+            let eids = try!(cache.get(&right_eid).ok_or(Error::MissingColumn(right_eid)));
+            let column = try!(db.cols.get(left).ok_or(Error::MissingColumn(left.to_owned())));
+
+            Ok((left_eid,
+                Filtered::Eids(find_eids_by_set(&column.entries, &eids))))
         }
     }
 }
 
-// FIXME: Remove needless result clonning
-fn exec_node(db: &Db, cache: &Cache, node: &QueryNode) -> Result<(ColumnName, Entries), Error> {
-    match *node {
-        QueryNode::Select(ref name) => {
-            let name_eid = name.eid();
-
-            // FIXME: Returns the results of the last where search
-            if let Some(entries) = cache.get(&name_eid) {
-                Ok((name.to_owned(), entries.clone()))
-            } else {
-                Err(Error::MissingColumn(name_eid))
-            }
-        }
-        QueryNode::Where(Lhs::Column(ref left), ref comp, Rhs::Constant(ref v)) => {
-            let left_eid = left.eid();
-
-            if let Some(entries) = cache.get(&left_eid) {
-                Ok((left_eid, filter_entries(entries, comp, v)))
-            } else if let Some(column) = db.cols.get(left) {
-                Ok((left_eid, filter_entries(&column.entries, comp, v)))
-            } else {
-                Err(Error::MissingColumn(left_eid))
-            }
-        }
-        QueryNode::Where(Lhs::Column(ref left), ref comp, Rhs::Column(ref right)) => panic!(),
-    }
+fn insert_or_merge(cache: &mut Cache, name: ColumnName, eids: Eids) {
+    let set = match cache.get(&name) {
+        Some(set) => set.clone(),
+        None => Eids::new(),
+    };
+    let set = eids.intersection(&set).cloned().collect();
+    cache.insert(name, set);
 }
 
 pub fn exec(db: &Db, plan: &Plan) -> Result<Vec<(ColumnName, Entries)>, Error> {
@@ -73,15 +132,13 @@ pub fn exec(db: &Db, plan: &Plan) -> Result<Vec<(ColumnName, Entries)>, Error> {
 
     let stage_nodes = plan.stage_nodes();
 
-    for (index, stage) in stage_nodes.iter().enumerate() {
+    for stage in &stage_nodes {
         for query_node in stage {
-            let (name, filtered) = try!(exec_node(db, &cache, query_node));
+            let (name, filtered) = try!(find_entries(db, &cache, query_node));
 
-            if index == stage_nodes.len() - 1 {
-                result.push((name, filtered));
-            } else {
-                cache.insert(name, filtered);
-                println!("cache: {:?}", cache);
+            match filtered {
+                Filtered::Eids(eids) => insert_or_merge(&mut cache, name, eids),
+                Filtered::Entries(entries) => result.push((name, entries)),
             }
         }
     }
